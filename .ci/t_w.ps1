f45115986970c05591d5d3c43861d5c55e7cf4d6
TP Boudreau <tpboudreau@gmail.com>
2020-11-12 20:12:55 +0000

    more logging msgs

--- .ci/t_w.ps1
+++ .ci/t_w.ps1
@@ -1,6 +1,8 @@
 function Check-Output {
   param( [bool]$success )
+  Write-Output "Testing output"
   if (!$success) {
+    Write-Output "Setting EXIT"
     $host.SetShouldExit(-1)
     Exit -1
   }
@@ -21,23 +23,24 @@ conda create -q -y -n $env:CONDA_ENV python=$env:PYTHON_VERSION joblib matplotli
 conda activate $env:CONDA_ENV
 
 # Install the Intel CPU runtime, so we can run tests against OpenCL
-Write-Output "Downloading OpenCL runtime"
+#Write-Output "Downloading OpenCL runtime"
 ###curl -o opencl_runtime_18.1_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13794/opencl_runtime_18.1_x64_setup.msi
 ###$msiarglist = "/i opencl_runtime_18.1_x64_setup.msi /quiet /norestart /log msi.log"
-curl -o opencl_runtime_16.1.2_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/12512/opencl_runtime_16.1.2_x64_setup.msi
-$msiarglist = "/i opencl_runtime_16.1.2_x64_setup.msi /quiet /norestart /log msi.log"
-Write-Output "Installing OpenCL runtime"
-$return = Start-Process msiexec -ArgumentList $msiarglist -Wait -passthru
-Get-Content msi.log
-If (@(0,3010) -contains $return.exitcode) {
-  Write-Output "OpenCL install successful"
-} else {
-  Write-Output "OpenCL install failed, aborting"
-  exit 1
-}
-RefreshEnv
+#curl -o opencl_runtime_16.1.2_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/12512/opencl_runtime_16.1.2_x64_setup.msi
+#$msiarglist = "/i opencl_runtime_16.1.2_x64_setup.msi /quiet /norestart /log msi.log"
+#Write-Output "Installing OpenCL runtime"
+#$return = Start-Process msiexec -ArgumentList $msiarglist -Wait -passthru
+#Get-Content msi.log
+#If (@(0,3010) -contains $return.exitcode) {
+#  Write-Output "OpenCL install successful"
+#} else {
+#  Write-Output "OpenCL install failed, aborting"
+#  exit 1
+#}
+#RefreshEnv
 Write-Output "Current OpenCL drivers Y:"
 Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
+#Start-Sleep -Seconds 10
 
 # TEMPORARY for debugging
 #Write-Output "Interrogating OpenCL runtime"
@@ -76,4 +79,5 @@ $env:LIGHTGBM_TEST_DUAL_CPU_GPU = "1"
 Write-Output "Running tests"
 pytest $tests ; Check-Output $?
 Write-Output "Completed tests"
+#Start-Sleep -Seconds 300
 
