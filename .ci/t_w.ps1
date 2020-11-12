2e0350e0eeb238f20edcc760dcafc40a5e97608d
TP Boudreau <tpboudreau@gmail.com>
2020-11-12 21:15:27 +0000

    install ocl, refresh, change config.h

--- .ci/t_w.ps1
+++ .ci/t_w.ps1
@@ -26,18 +26,18 @@ conda activate $env:CONDA_ENV
 #Write-Output "Downloading OpenCL runtime"
 ###curl -o opencl_runtime_18.1_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13794/opencl_runtime_18.1_x64_setup.msi
 ###$msiarglist = "/i opencl_runtime_18.1_x64_setup.msi /quiet /norestart /log msi.log"
-#curl -o opencl_runtime_16.1.2_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/12512/opencl_runtime_16.1.2_x64_setup.msi
-#$msiarglist = "/i opencl_runtime_16.1.2_x64_setup.msi /quiet /norestart /log msi.log"
-#Write-Output "Installing OpenCL runtime"
-#$return = Start-Process msiexec -ArgumentList $msiarglist -Wait -passthru
-#Get-Content msi.log
-#If (@(0,3010) -contains $return.exitcode) {
-#  Write-Output "OpenCL install successful"
-#} else {
-#  Write-Output "OpenCL install failed, aborting"
-#  exit 1
-#}
-#RefreshEnv
+curl -o opencl_runtime_16.1.2_x64_setup.msi http://registrationcenter-download.intel.com/akdlm/irc_nas/12512/opencl_runtime_16.1.2_x64_setup.msi
+$msiarglist = "/i opencl_runtime_16.1.2_x64_setup.msi /quiet /norestart /log msi.log"
+Write-Output "Installing OpenCL runtime"
+$return = Start-Process msiexec -ArgumentList $msiarglist -Wait -passthru
+Get-Content msi.log
+If (@(0,3010) -contains $return.exitcode) {
+  Write-Output "OpenCL install successful"
+} else {
+  Write-Output "OpenCL install failed, aborting"
+  exit 1
+}
+RefreshEnv
 Write-Output "Current OpenCL drivers:"
 Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
 #Start-Sleep -Seconds 10
