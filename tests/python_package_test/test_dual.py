33e1e0851c4a8b2a9db0b48b8a47176b4088acc4
TP Boudreau <tpboudreau@gmail.com>
2020-11-16 21:02:18 +0000

    add msgs

--- tests/python_package_test/test_dual.py
+++ tests/python_package_test/test_dual.py
@@ -32,7 +32,9 @@ def test_gpu_works():
     label = np.random.randint(2, size=500)
     validation_data = train_data = lgb.Dataset(data, label=label)
 
+    print("A")
     try:
+        print("B")
         #param = {"num_leaves": 31, "objective": "binary", "device": "gpu", 'gpu_use_dp': True}
         #param = {"num_leaves": 31, "objective": "binary", "device": "gpu"}
         #gbm = lgb.train(param, train_data, 10, valid_sets=[validation_data])
@@ -49,10 +51,17 @@ def test_gpu_works():
         }
         gbm = lgb.Booster(params, train_data)
     except LightGBMError as e:
+        print("C")
         if str(e) == "No OpenCL device found":
             # This is fine, it means there's no OpenCL device available,
             # and OpenCL device is only searched for if we successfully
             # loaded OpenCL GPU backend.
-            pass
+            print("C.1")
+            #pass
         else:
+            print("C.2")
             raise
+    else:
+        print("D")
+        #pass
+    print("E")
