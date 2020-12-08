ba433c8366b6fbb5af12d3b303469599ac152a4e
TP Boudreau <tpboudreau@gmail.com>
2020-12-08 20:41:33 +0000

    run w/o pytest

--- .ci/t_p.ps1
+++ .ci/t_p.ps1
@@ -29,10 +29,10 @@ cp @(Get-ChildItem *.whl) $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 
 Write-Output "Running tests"
 #$tests = $env:BUILD_SOURCESDIRECTORY + "/tests"
-$env:LIGHTGBM_TEST_DUAL_CPU_GPU = "1"
+#$env:LIGHTGBM_TEST_DUAL_CPU_GPU = "1"
 cd $env:BUILD_SOURCESDIRECTORY/tests/python_package_test
-#python test_dual.py ; Check-Output $?
-pytest test_dual.py ; Check-Output $?
+python test_dual.py ; Check-Output $?
+#pytest test_dual.py ; Check-Output $?
 #pytest $tests ; Check-Output $?
 Write-Output "Completed tests"
 
--- tests/python_package_test/test_dual.py
+++ tests/python_package_test/test_dual.py
@@ -1,18 +1,18 @@
 """Tests for dual GPU+CPU support."""
 
 import os
-#import sys
-import pytest
+import sys
+#import pytest
 
 import lightgbm as lgb
 import numpy as np
 from lightgbm.basic import LightGBMError
 
 
-@pytest.mark.skipif(
-    os.environ.get("LIGHTGBM_TEST_DUAL_CPU_GPU", None) is None,
-    reason="Only run if appropriate env variable is set",
-)
+#@pytest.mark.skipif(
+#    os.environ.get("LIGHTGBM_TEST_DUAL_CPU_GPU", None) is None,
+#    reason="Only run if appropriate env variable is set",
+#)
 def test_cpu_works():
     """If compiled appropriately, the same installation will support both GPU and CPU."""
     data = np.random.rand(500, 10)
@@ -23,10 +23,10 @@ def test_cpu_works():
     gbm = lgb.train(param, train_data, 10, valid_sets=[validation_data])
 
 
-@pytest.mark.skipif(
-    os.environ.get("LIGHTGBM_TEST_DUAL_CPU_GPU", None) is None,
-    reason="Only run if appropriate env variable is set",
-)
+#@pytest.mark.skipif(
+#    os.environ.get("LIGHTGBM_TEST_DUAL_CPU_GPU", None) is None,
+#    reason="Only run if appropriate env variable is set",
+#)
 def test_gpu_works():
     """If compiled appropriately, the same installation will support both GPU and CPU."""
     data = np.random.rand(500, 10)
@@ -37,23 +37,23 @@ def test_gpu_works():
     gbm = lgb.train(param, train_data, 10, valid_sets=[validation_data])
 
 
-#if __name__ == "__main__":
-#    print('main()')
-#    try:
-#        print('CPU ...')
-#        test_cpu_works()
-#    except Exception as e:
-#        print('CPU run FAILED')
-#        print(e)
-#        sys.exit(-1)
-#
-#    try:
-#        print('GPU ...')
-#        test_gpu_works()
-#    except Exception as e:
-#        print('GPU run FAILED')
-#        print(e)
-#        sys.exit(-1)
-#
-#    print('exit(0)')
-#    sys.exit(0)
+if __name__ == "__main__":
+    print('main()')
+    try:
+        print('CPU ...')
+        test_cpu_works()
+    except Exception as e:
+        print('CPU run FAILED')
+        print(e)
+        sys.exit(-1)
+
+    try:
+        print('GPU ...')
+        test_gpu_works()
+    except Exception as e:
+        print('GPU run FAILED')
+        print(e)
+        sys.exit(-1)
+
+    print('exit(0)')
+    sys.exit(0)
