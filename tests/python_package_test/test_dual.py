0de71d77d05af053d4adbc4bc958c70f6359d049
TP Boudreau <tpboudreau@gmail.com>
2020-12-08 19:24:17 +0000

    another try

--- tests/python_package_test/test_dual.py
+++ tests/python_package_test/test_dual.py
@@ -1,7 +1,7 @@
 """Tests for dual GPU+CPU support."""
 
 import os
-import pytest
+#import pytest
 
 import lightgbm as lgb
 import numpy as np
@@ -14,12 +14,13 @@ from lightgbm.basic import LightGBMError
 #)
 def test_cpu_works():
     """If compiled appropriately, the same installation will support both GPU and CPU."""
+    print('... CPU ...')
     data = np.random.rand(500, 10)
     label = np.random.randint(2, size=500)
     validation_data = train_data = lgb.Dataset(data, label=label)
 
     param = {"num_leaves": 31, "objective": "binary", "device": "cpu"}
-    lgb.train(param, train_data, 10, valid_sets=[validation_data])
+    gbm = lgb.train(param, train_data, 10, valid_sets=[validation_data])
 
 
 #@pytest.mark.skipif(
@@ -28,27 +29,32 @@ def test_cpu_works():
 #)
 def test_gpu_works():
     """If compiled appropriately, the same installation will support both GPU and CPU."""
+    print('... GPU ...')
     data = np.random.rand(500, 10)
     label = np.random.randint(2, size=500)
     validation_data = train_data = lgb.Dataset(data, label=label)
 
     param = {"num_leaves": 31, "objective": "binary", "device": "gpu"}
-    lgb.train(param, train_data, 10, valid_sets=[validation_data])
+    gbm = lgb.train(param, train_data, 10, valid_sets=[validation_data])
 
 
 if __name__ == "__main__":
+    print('main()')
     try:
+        print('CPU ...')
         test_cpu_works()
     except Exception as e:
         print('CPU run FAILED')
-        sys.exit(-1)
         print(e)
+        sys.exit(-1)
 
     try:
+        print('GPU ...')
         test_gpu_works()
     except Exception as e:
         print('GPU run FAILED')
         print(e)
         sys.exit(-1)
 
+    print('exit(0)')
     sys.exit(0)
