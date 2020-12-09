4ebaeddbedfd0330f1afd8b14ab0a6def7b8c67d
TP Boudreau <tpboudreau@gmail.com>
2020-12-09 02:48:34 +0000

    invoke-command install; all tests

--- .ci/t_p.ps1
+++ .ci/t_p.ps1
@@ -12,33 +12,32 @@ Write-Output "Interrogating OpenCL runtime"
 curl https://ci.appveyor.com/api/projects/oblomov/clinfo/artifacts/clinfo.exe?job=platform%3a+x64 -o clinfo.exe
 .\clinfo.exe
 
-# setup for Python
-#Write-Output "Setting up conda environment"
-#conda init powershell
-#conda activate
-#conda config --set always_yes yes --set changeps1 no
-#conda update -q -y conda
-#conda create -q -y -n $env:CONDA_ENV python=$env:PYTHON_VERSION joblib matplotlib numpy pandas psutil pytest pytest-timeout python-graphviz scikit-learn scipy ; Check-Output $?
-#conda activate $env:CONDA_ENV
+Write-Output "Setting up conda environment"
+conda init powershell
+conda activate
+conda config --set always_yes yes --set changeps1 no
+conda update -q -y conda
+conda create -q -y -n $env:CONDA_ENV python=$env:PYTHON_VERSION joblib matplotlib numpy pandas psutil pytest pytest-timeout python-graphviz scikit-learn scipy ; Check-Output $?
+conda activate $env:CONDA_ENV
 
-#Write-Output "Building and installing wheel"
-#cd $env:BUILD_SOURCESDIRECTORY/python-package
-#python setup.py bdist_wheel --integrated-opencl --plat-name=win-amd64 --universal ; Check-Output $?
-#cd dist; pip install --user @(Get-ChildItem *.whl) ; Check-Output $?
-#cp @(Get-ChildItem *.whl) $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+Write-Output "Building and installing wheel"
+cd $env:BUILD_SOURCESDIRECTORY/python-package
+python setup.py bdist_wheel --integrated-opencl --plat-name=win-amd64 --universal ; Check-Output $?
+cd dist; pip install --user @(Get-ChildItem *.whl) ; Check-Output $?
+cp @(Get-ChildItem *.whl) $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 
 #Write-Output "Running module"
 #cd $env:BUILD_SOURCESDIRECTORY/tests/python_package_test
 #python test_dual.py ; Check-Output $?
 #Write-Output "Completed module"
 
-#Write-Output "Running tests"
-#$tests = $env:BUILD_SOURCESDIRECTORY + "/tests"
-#$env:LIGHTGBM_TEST_DUAL_CPU_GPU = "1"
+Write-Output "Running tests"
+$tests = $env:BUILD_SOURCESDIRECTORY + "/tests"
+$env:LIGHTGBM_TEST_DUAL_CPU_GPU = "1"
 ##cd $env:BUILD_SOURCESDIRECTORY/tests/python_package_test
 ##pytest test_dual.py ; Check-Output $?
-#pytest $tests ; Check-Output $?
-#Write-Output "Completed tests"
+pytest $tests ; Check-Output $?
+Write-Output "Completed tests"
 
-#conda deactivate
+conda deactivate
 
