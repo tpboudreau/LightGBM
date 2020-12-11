5a79126c46f10eddbbfad6d665b57acb83bca794
TP Boudreau <tpboudreau@gmail.com>
2020-12-11 15:42:10 +0000

    full download; hardcode name 2

--- .ci/i_o.ps1
+++ .ci/i_o.ps1
@@ -3,17 +3,10 @@ Write-Output "Agent platform information:"
 Get-WmiObject -Class Win32_ComputerSystem
 Get-WmiObject -Class Win32_Processor
 Get-WmiObject -Class Win32_BIOS
-#(Get-Host).Version
 
 Write-Output "Downloading OpenCL runtime"
 #curl -o AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe http://amd-dev.wpengine.netdna-cdn.com/app-sdk/installers/APPSDKInstaller/3.0.130.135-GA/full/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
-#
-#$installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
-#curl https://gamma-rho.com/$installer --ipv4 --tlsv1.2 --output $installer
-#Invoke-WebRequest -OutFile $installer -Uri https://gamma-rho.com/$installer
 Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe -Uri https://gamma-rho.com/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
-#
-#Invoke-WebRequest -OutFile clinfo.exe -Uri https://gamma-rho.com/clinfo-windows-x64.exe
 
 Write-Output "Installing OpenCL runtime"
 Invoke-Command -ScriptBlock {Start-Process 'AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe' -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
