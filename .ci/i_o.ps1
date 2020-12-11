95a2deedd95446f6aa2621a47ba0ed35a6d3b7e3
TP Boudreau <tpboudreau@gmail.com>
2020-12-11 17:03:39 +0000

    show .net version then bail

--- .ci/i_o.ps1
+++ .ci/i_o.ps1
@@ -4,6 +4,8 @@ Get-WmiObject -Class Win32_ComputerSystem
 Get-WmiObject -Class Win32_Processor
 Get-WmiObject -Class Win32_BIOS
 
+Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full'
+
 Write-Output "Downloading OpenCL runtime"
 #curl -o AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe http://amd-dev.wpengine.netdna-cdn.com/app-sdk/installers/APPSDKInstaller/3.0.130.135-GA/full/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
 Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe -Uri https://gamma-rho.com/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
@@ -21,5 +23,6 @@ if ($property -eq $null) {
   Write-Output "Successfully installed OpenCL runtime"
   Write-Output "Current OpenCL drivers:"
   Write-Output $property
+  Exit -1
 }
 
