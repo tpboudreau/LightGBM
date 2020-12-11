66a6749da57f64bdb5d0793370a403f7d52f4fa7
TP Boudreau <tpboudreau@gmail.com>
2020-12-11 21:38:53 +0000

    download/combine split file loop 5

--- .ci/i_o.ps1
+++ .ci/i_o.ps1
@@ -10,25 +10,17 @@ Get-WmiObject -Class Win32_BIOS
 #curl -o AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe http://amd-dev.wpengine.netdna-cdn.com/app-sdk/installers/APPSDKInstaller/3.0.130.135-GA/full/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
 #Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe -Uri https://gamma-rho.com/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
 
-#Write-Output "Downloading OpenCL runtime"
-#Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.1 -Uri https://gamma-rho.com/split/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.1
-#Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.2 -Uri https://gamma-rho.com/split/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.2
-#Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.3 -Uri https://gamma-rho.com/split/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.3
-#Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.4 -Uri https://gamma-rho.com/split/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.4
-#Invoke-WebRequest -OutFile AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.EXE -Uri https://gamma-rho.com/split/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.EXE
-#.\AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.EXE
-
 Write-Output "Downloading OpenCL runtime"
 $parts = @("1", "2", "3", "4", "EXE")
 foreach ($p in $parts) {
   Write-Output " - downloading part $($p)"
   Invoke-WebRequest -OutFile "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.$($p)" -Uri "https://gamma-rho.com/split/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.$($p)"
 }
+Get-ChildItem .\AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.EXE
 Write-Output " - combining parts"
 Start-Process ".\AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe.EXE"
 Write-Output $?
-Write-Output "DONE"
-dir
+Get-ChildItem .\AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
 
 Write-Output "Installing OpenCL runtime"
 Invoke-Command -ScriptBlock {Start-Process '.\AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe' -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
