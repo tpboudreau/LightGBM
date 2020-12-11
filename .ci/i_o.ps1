7519fed4ea5a736c2505669b81508d0567f0e9b6
TP Boudreau <tpboudreau@gmail.com>
2020-12-11 07:51:34 +0000

    use invoke-webrequest explicitly 4

--- .ci/i_o.ps1
+++ .ci/i_o.ps1
@@ -3,14 +3,15 @@ Write-Output "Agent platform information:"
 Get-WmiObject -Class Win32_ComputerSystem
 Get-WmiObject -Class Win32_Processor
 Get-WmiObject -Class Win32_BIOS
+(Get-Host).Version
 
 Write-Output "Downloading OpenCL runtime"
 #curl -o AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe http://amd-dev.wpengine.netdna-cdn.com/app-sdk/installers/APPSDKInstaller/3.0.130.135-GA/full/AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe
 
 $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
 #curl https://gamma-rho.com/$installer --ipv4 --tlsv1.2 --output $installer
-#Invoke-WebRequest -SslProtocol Tls12 -OutFile $installer -Uri https://gamma-rho.com/$installer
-Invoke-WebRequest -OutFile $installer -Uri https://gamma-rho.com/$installer
+Invoke-WebRequest -SslProtocol Tls12 -OutFile $installer -Uri https://gamma-rho.com/$installer
+#Invoke-WebRequest -OutFile $installer -Uri https://gamma-rho.com/$installer
 
 Write-Output "Installing OpenCL runtime"
 Invoke-Command -ScriptBlock {Start-Process .\$installer -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
