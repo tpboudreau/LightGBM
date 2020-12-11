dec45a16b1dc8ee8ab2eb8c6a00189a478c00acb
TP Boudreau <tpboudreau@gmail.com>
2020-12-11 02:07:11 +0000

    test intentional installation failure

--- .ci/i_o.ps1
+++ .ci/i_o.ps1
@@ -11,8 +11,17 @@ $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
 curl -o .\$installer https://gamma-rho.com/$installer
 
 Write-Output "Installing OpenCL runtime"
-Invoke-Command -ScriptBlock {Start-Process .\$installer -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
+##Invoke-Command -ScriptBlock {Start-Process .\$installer -ArgumentList '/S /V"/quiet /norestart /passive /log amd_opencl_sdk.log"' -Wait}
 
-Write-Output "Current OpenCL drivers:"
-Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
+$property = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
+if ($property -eq $null) {
+  Write-Output "Unable to install OpenCL runtime"
+  Write-Output "Setting EXIT"
+  $host.SetShouldExit(-1)
+  Exit -1
+} else {
+  Write-Output "Successfully installed OpenCL runtime"
+  Write-Output "Current OpenCL drivers:"
+  Write-Output $property
+}
 
