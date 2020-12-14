061df6dc83f7ef6b9660cdddbb89db80dba06f7f
TP Boudreau <tpboudreau@gmail.com>
2020-12-14 18:26:12 +0000

    new download site

--- .ci/d_o.ps1
+++ .ci/d_o.ps1
@@ -2,20 +2,22 @@
 $cache = "$env:PIPELINE_WORKSPACE\opencl_windows-amd_cpu-v3_0_130_135"
 $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
 
-Write-Output "Downloading OpenCL installer parts"
+Write-Output "Downloading ..."
 $parts = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "EXE")
 foreach ($p in $parts) {
   Write-Output " - downloading part $($p)"
-  Invoke-WebRequest -OutFile "$installer.$p" -Uri "https://gamma-rho.com/parts/$installer.$p"
+  Invoke-WebRequest -OutFile "$installer.$p" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer.$p"
 }
-Write-Output "Combining parts"
+Write-Output "Reassembling ..."
 Start-Process "$installer.EXE" -Wait
 Start-Sleep -Seconds 10
+
+Write-Output "Caching ... "
 New-Item $cache -ItemType Directory | Out-Null
 Move-Item -Path "$installer" -Destination "$cache\$installer" | Out-Null
 
 if (Test-Path "$cache\$installer") {
-  Write-Output "Successfully downloaded OpenCL ..."
+  Write-Output "Successfully downloaded ..."
 } else {
   Write-Output "Unable to download ..."
   Write-Output "Setting EXIT"
