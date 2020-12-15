a9056953a2fd6ec23d78d7374b50b09b8e2c4c57
TP Boudreau <tpboudreau@gmail.com>
2020-12-15 01:08:58 +0000

    download full file

--- .ci/d_o.ps1
+++ .ci/d_o.ps1
@@ -2,15 +2,18 @@
 $cache = "$env:PIPELINE_WORKSPACE\opencl_windows-amd_cpu-v3_0_130_135"
 $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
 
+#Write-Output "Downloading ..."
+#$parts = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "EXE")
+#foreach ($p in $parts) {
+#  Write-Output " - downloading part $($p)"
+#  Invoke-WebRequest -OutFile "$installer.$p" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer.$p"
+#}
+#Write-Output "Reassembling ..."
+#Start-Process "$installer.EXE" -Wait
+#Start-Sleep -Seconds 10
+
 Write-Output "Downloading ..."
-$parts = @("1", "2", "3", "4", "5", "6", "7", "8", "9", "EXE")
-foreach ($p in $parts) {
-  Write-Output " - downloading part $($p)"
-  Invoke-WebRequest -OutFile "$installer.$p" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer.$p"
-}
-Write-Output "Reassembling ..."
-Start-Process "$installer.EXE" -Wait
-Start-Sleep -Seconds 10
+Invoke-WebRequest -OutFile "$installer" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer"
 
 Write-Output "Caching ... "
 New-Item $cache -ItemType Directory | Out-Null
