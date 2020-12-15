6470d82e12a80912ee163c7350a88c2ae5832058
TP Boudreau <tpboudreau@gmail.com>
2020-12-15 19:48:08 +0000

    cache hit on 8; skip download in script

--- .ci/i_o.ps1
+++ .ci/i_o.ps1
@@ -4,9 +4,29 @@ Get-WmiObject -Class Win32_ComputerSystem
 Get-WmiObject -Class Win32_Processor
 Get-WmiObject -Class Win32_BIOS
 
-Write-Output "Installing OpenCL runtime"
 $cache = "$env:PIPELINE_WORKSPACE\opencl_windows-amd_cpu-v3_0_130_135"
 $installer = "AMD-APP-SDKInstaller-v3.0.130.135-GA-windows-F-x64.exe"
+
+if ($env:OPENCL_INSTALLER_FOUND -ne 'true')) {
+
+  Write-Output "Downloading ..."
+  Invoke-WebRequest -OutFile "$installer" -Uri "https://github.com/microsoft/LightGBM/releases/download/v2.0.12/$installer"
+
+  Write-Output "Caching ... "
+  New-Item $cache -ItemType Directory | Out-Null
+  Move-Item -Path "$installer" -Destination "$cache\$installer" | Out-Null
+
+  if (Test-Path "$cache\$installer") {
+    Write-Output "Successfully downloaded ..."
+  } else {
+    Write-Output "Unable to download ..."
+    Write-Output "Setting EXIT"
+    $host.SetShouldExit(-1)
+    Exit -1
+  }
+}
+
+Write-Output "Installing OpenCL runtime"
 Invoke-Command -ScriptBlock {Start-Process "$cache\$installer" -ArgumentList '/S /V"/quiet /norestart /passive /log opencl.log"' -Wait}
 
 $property = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenCL\Vendors
